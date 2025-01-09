b5618ad31ef0445121ec4f1b0342331af540ab6b
Peaklatice-9348 <yusufjavaid.072012@gmail.com>
2025-01-09 19:05:32 +0000

    Add files via upload

--- card/homework.py
+++ card/homework.py
@@ -3,6 +3,7 @@ import sys
 pygame.init()
 WIDTH = 500
 HEIGHT = 500
+timer = 100
 
 class Rect():
     def __init__(self,x,y,color,radius,width):
@@ -13,7 +14,8 @@ class Rect():
         self.radius=radius
         self.width=width
     def draw(self):
-        pygame.draw.rect(self.screen,self.color,(self.x,self.y,self.radius*5,self.radius),border_radius=self.width)
+        pygame.draw.rect(self.screen,'black',(self.x,self.y,self.radius*5,self.radius))
+        pygame.draw.rect(self.screen,self.color,(self.x+5,self.y+5,self.radius*5-10,self.radius-10),)
         
 class Circle():
     def __init__(self,x,y,color,radius,width):
@@ -25,11 +27,11 @@ class Circle():
         self.radius=radius
         self.width=width
     def draw(self):
-        self.screen.fill('blue')
-        pygame.draw.circle(self.screen,self.color,(self.x,self.y),self.radius,)
+        self.screen.fill('white')
+        pygame.draw.circle(self.screen,self.color,(self.x,self.y),self.radius)
         pygame.draw.circle(self.screen,'black',(self.x,self.y),self.radius,self.width)
         
-blub = Circle(WIDTH/2,HEIGHT/2,'grey',100,5)
+blub = Circle(WIDTH/2,HEIGHT/2-100,'grey',100,5)
 switch = Rect(WIDTH/2-125,HEIGHT-HEIGHT/4,'White',50,10)
 
 running = True
@@ -37,8 +39,27 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if timer != 0:
+            timer = timer - 1
+                    
+
+
     blub.draw()
     switch.draw()
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        x,y = pygame.mouse.get_pos()
+        if x > 125 and x < 375:
+            if y < 425 and y > 375:
+                if timer == 0:
+                    timer = 10
+                    if blub.color == 'grey':
+                        blub.color = 'yellow'
+                    elif blub.color == 'yellow':
+                        blub.color = 'grey'
+        
+                    
+
+
     pygame.display.flip()
 pygame.quit()
 sys.exit()
